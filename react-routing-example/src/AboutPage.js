b8ce0069c3aaaf0779b362dae2301db58a5bb6e7
N Naman Kumar <namanreddy09@gmail.com>
2025-03-05 21:00:05 +0530

    react_routing

---
+++ react-routing-example/src/AboutPage.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function AboutPage() {
+  return (
+    <div className="about-page">
+      <h1>About Us</h1>
+      <p>This is the about page of the React app.</p>
+    </div>
+  );
+}
+
+export default AboutPage;
